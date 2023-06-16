0a24cd90b6b8dd0267b09373398a0dbb79f6e303
Saidamirzoda Mujtabo <s.mujtabo05@gmail.com>
2023-06-16 15:01:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "/bW0H0TuUo05T3t2gJW2SA==sONteIg77fH5MH9l";
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-key": apiKey,
+  },
+};
+
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "Something went wrong. Try again later.";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
